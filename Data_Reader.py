f4a994bd91a7eb49eeb97b8c603a3b34abe29688
Minji Kang <kyang@snu.ac.kr>
2019-03-28 12:18:41 +0900

    parameterize train-test boundary

--- Data_Reader.py
+++ Data_Reader.py
@@ -5,15 +5,17 @@ import numpy as np
 def read_data(source="./data/pathway_activity.csv"):
     xy = np.loadtxt(source, delimiter=',', dtype=np.float32)
 
-    x_train = xy[:913, :-1]
-    x_test = xy[913:, :-1]
+    boundary = len(xy) * 9 // 10
+
+    x_train = xy[:boundary, :-1]
+    x_test = xy[boundary:, :-1]
 
     # Encode label(0, 1, 2, 3) to one-hot
     label = xy[:, [-1]].flatten().astype(int)
     y = np.zeros((label.size, 4))
     y[np.arange(label.size), label] = 1
 
-    y_train = y[:913, :]
-    y_test = y[913:, :]
+    y_train = y[:boundary, :]
+    y_test = y[boundary:, :]
 
     return x_train, y_train, x_test, y_test
